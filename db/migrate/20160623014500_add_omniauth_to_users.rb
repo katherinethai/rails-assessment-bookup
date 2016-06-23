aa5aff2c65063960984bd21dc69b5160afb9ccea
katherinethai <kthai1618@gmail.com>
2016-06-22 21:53:10 -0400

    i think this fixed my database?

--- db/migrate/20160623014500_add_omniauth_to_users.rb
+++ db/migrate/20160623014500_add_omniauth_to_users.rb
@@ -1,7 +1,5 @@
 class AddOmniauthToUsers < ActiveRecord::Migration
   def change
-    remove_column :users, :provider
-    remove_column :users, :uid
     add_column :users, :provider, :string
     add_column :users, :uid, :string
   end
